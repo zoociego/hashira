ba4b4f369ad9bcd659df50f0d5528b5abf123f38
La Pucelle <brunodeimian@gmail.com>
2022-11-24 06:22:15 -0300

    Diseño de News, Nuevo logo

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,67 +1,65 @@
 import NavBar from '../Home/Nav/navBar.jsx'
-import Layout from '../Home/Layout/Layout.jsx'
 import React from 'react'
 import './News.css'
 
 const News = () => {
   return (
         <>
-        <Layout />
+        <div className='Wallpaper'>
         <div className='ContenedorPrincipal'>
-            <ul className='ContenedorSecunario'>
-                <li className='Containers'>
-                    <div className='ContainerImage'><img alt='a' className='Noticebg' src="https://wallpapercrafter.com/th800/132959-Hytale-RPG-video-games-video-game-art.jpg"/></div>
-                    <a className='title'>
-                        <h3>Hashira Ro Patch 1.54</h3>
-                        <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
-                    </a>
-                    <br />
-                    <div className='Foot'>
-                        <span className='date'>29/09/2022</span>
-                        <span className='Notice'>Noticias</span>
-                    </div>
-                </li>
+                <ul className='ContenedorSecunario'>
+                    <li className='Containers'>
+                        <div className='ContainerImage'><img alt='a' className='Noticebg' src="https://wallpapercrafter.com/th800/132959-Hytale-RPG-video-games-video-game-art.jpg"/></div>
+                        <a className='title'>
+                            <h3>Hashira Ro Patch 1.54</h3>
+                            <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
+                        </a>
+                        <br />
+                        <div className='Foot'>
+                            <span className='date'>29/09/2022</span>
+                            <span className='Notice'>Noticias</span>
+                        </div>
+                    </li>
 
-                <li className='Containers'>
-                    <div className='ContainerImag-e'><img alt='a' className='Noticebg' src="https://wallpaperaccess.com/full/636544.jpg"/></div>
-                    <a className='title'>
-                        <h3>Hashira Ro Patch 1.54</h3>
-                        <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en imagen, este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
-                    </a>
-                    <br />
-                    <div className='Foot'>
-                        <span className='date'>29/09/2022</span>
-                        <span className='Notice'>Noticias</span>
-                    </div>
-                </li>
-
-                <li className='Containers'>
-                    <div className='ContainerImage'><img alt='a' className='Noticebg' src="https://wallpapercrafter.com/th800/132962-Hytale-RPG-video-games-video-game-art.jpg"/></div>
-                    <a className='title'>
-                        <h3>Hashira Ro Patch 1.54</h3>
-                        <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en imagen, este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
-                    </a>
-                    <br />
-                    <div className='Foot'>
-                        <span className='date'>29/09/2022</span>
-                        <span className='Notice'>Noticias</span>
-                    </div>
-                </li>
-            </ul>
-            <br />
-            <br />
-            <div className='categoryBox'>
-                <li className='Category'>Mas Recientes</li>
-                <li className='Category'>Noticias</li>
-                <li className='Category'>Eventos</li>
+                    <li className='Containers'>
+                        <div className='ContainerImag-e'><img alt='a' className='Noticebg' src="https://wallpaperaccess.com/full/636544.jpg"/></div>
+                        <a className='title'>
+                            <h3>Hashira Ro Patch 1.54</h3>
+                            <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en imagen, este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
+                        </a>
+                        <br />
+                        <div className='Foot'>
+                            <span className='date'>29/09/2022</span>
+                            <span className='Notice'>Noticias</span>
+                        </div>
+                    </li>
+                    <li className='Containers'>
+                        <div className='ContainerImage'><img alt='a' className='Noticebg' src="https://wallpapercrafter.com/th800/132962-Hytale-RPG-video-games-video-game-art.jpg"/></div>
+                        <a className='title'>
+                            <h3>Hashira Ro Patch 1.54</h3>
+                            <p className='body'>Al entrar a este mundo NPC ubicado en prontera mano izquierda como se muestra en imagen, este prohibirá el uso de equipos quest , vips , entre otras variables. para solo dar paso al uso de equipo de armeria en el cual se podrá comprar dentro del mismo mapa ( se parece a patricio estrella )</p>
+                        </a>
+                        <br />
+                        <div className='Foot'>
+                            <span className='date'>29/09/2022</span>
+                            <span className='Notice'>Noticias</span>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <br />
-            <br />
-
         </div>
-        <div className='news'>
-            </div>
+            <br />
+            <div className='containerBox'>
+                <div className='categoryBox'>
+                    <li className='Category'>Mas Recientes</li>
+                    <li className='Category'>Noticias</li>
+                    <li className='Category'>Eventos</li>
+                </div>
 
+                <div className='news'>
+
+                </div>
+            </div>
         <NavBar />
         </>
 
